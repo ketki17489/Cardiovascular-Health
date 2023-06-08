bbb8bde8195c41276eafe7737e2f3050d398afc3
Ketki <ketkimuzumdar@hotmail.com>
2023-06-08 22:20:02 +0200

    Create final_win.py
    Last window to display the Result.

---
+++ final_win.py
@@ -0,0 +1,21 @@
+class FinalWin(QWidget):
+  def __init__(self):
+    ''' the window which the greeting is located in '''
+    super().__init__()
+    # creating and configuring graphic elements:
+    self.initUI()
+    #establishes connections between elements
+    self.connects()
+    # sets what the window will look like (label, size, location)
+    self.set_appear()
+    # start:
+    self.show()
+   
+  def set_appear(self):
+    pass
+  def initUI(self):
+    pass
+  def connects(self):
+    pass
+  def show(self):
+    pass
